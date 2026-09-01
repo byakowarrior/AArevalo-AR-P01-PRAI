66b0e88d70670afd7caa090759156ca630477b88
ADRIAN AREVALO <draken1043@gmail.com>
2026-08-31 20:15:54 -0500

    Actually
    Clase adds

--- Assets/Scripts/CambioDeEscenas.cs
+++ Assets/Scripts/CambioDeEscenas.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CambioDeEscenas : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class CambioDeEscenas : MonoBehaviour
     {
-        
+        public void CargarEscena(string que_escena)
+        {
+            SceneManager.LoadScene(que_escena);
+        }
     }
-}
+  
